fd5a968386a2f8448770249c2c47315f220f8a81
aydar.gaysin@gmail.com <aydar.gaysin@gmail.com>
2021-04-14 13:00:51 +0300

    Added predict_rub_salary_sj(vacancy) method, edited parameters

--- superjob_api.py
+++ superjob_api.py
@@ -10,17 +10,44 @@ load_dotenv()
 SUPERJOB_API_URL = 'https://api.superjob.ru/2.20/vacancies/'
 
 headers = {
-    'X-Api-App-Id': os.getenv('API_SECRET_KEY'),
+    'X-Api-App-Id': os.getenv('SJ_API_SECRET_KEY'),
 }
 
 parameters = {
-    'order_field': 'date',
-    'order_direction': 'desc',
-    'count': '100'
+    'keyword': 'Программист',
+    'count': '100',
+    'town': 'Москва',
+    'catalogues': 48
 }
 
 response = requests.get(SUPERJOB_API_URL, headers=headers, params=parameters)
+#response = requests.get(SUPERJOB_API_URL, headers=headers)
 response.raise_for_status()
 api_response = response.json()
+
+vacancy = {}
+
+
+def predict_rub_salary_sj(vacancy):
+    if vacancy['currency'] == 'rub':
+        min_salary = vacancy['payment_from']
+        max_salary = vacancy['payment_to']
+        if min_salary is None and max_salary:
+            salary = max_salary * 0.8
+        elif min_salary and max_salary is None:
+            salary = min_salary * 1.2
+        elif min_salary and max_salary:
+            salary = (min_salary + max_salary) / 2
+        else:
+            salary = None
+    else:
+        salary = None
+    return salary
+
+
 for vacancy in api_response['objects']:
-    pprint(vacancy['profession'])
+    salary = predict_rub_salary_sj(vacancy)
+    pprint(f"{vacancy['profession']}, {vacancy['town']['title']}, {salary}")
+    #pprint(f"{vacancy['profession']}")
+    #pprint(vacancy)
+
