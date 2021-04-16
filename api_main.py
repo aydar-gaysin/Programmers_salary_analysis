68140c388ca317a2d5d4454ec61e9a67087f57a1
aydar.gaysin@gmail.com <aydar.gaysin@gmail.com>
2021-04-16 11:08:48 +0300

    Edited functions names, added global variables

--- api_main.py
+++ api_main.py
@@ -7,15 +7,17 @@ from terminaltables import AsciiTable
 
 SJ_API_URL = 'https://api.superjob.ru/2.20/vacancies/'
 HH_API_URL = 'https://api.hh.ru/vacancies/'
+MOSCOW_ID = '1'
+DEVELOPMENT_CATALOGUE_ID = 48
 PROGRAMMING_LANGUAGES = [
-    'C++', 'C#', 'Dart', 'Elexir', 'Go', 'Java', 'Javascript',
+    'C++', 'C#', 'Dart', 'Go', 'Java', 'Javascript',
     'Objective-C', 'PHP', 'Python', 'Ruby', 'Scala', 'Swift', 'Typescript'
 ]
 
 
 def load_hh_vacancies(hh_api_url, language):
     parameters = {
-        'area': '1',
+        'area': MOSCOW_ID,
         'text': language,
     }
     response = requests.get(f'{hh_api_url}', params=parameters)
@@ -34,7 +36,7 @@ def load_sj_vacancies(sj_api_key, sj_api_url, programming_language, sj_page_numb
         'count': '100',
         'page': sj_page_number,
         'town': 'Москва',
-        'catalogues': 48
+        'catalogues': DEVELOPMENT_CATALOGUE_ID
     }
     response = requests.get(sj_api_url, headers=headers, params=parameters)
     response.raise_for_status()
@@ -80,7 +82,7 @@ def predict_rub_salary_hh(salary_data):
     return salary
 
 
-def get_sj_vacancies_data(sj_api_key):
+def get_sj_vacancies(sj_api_key):
     vacancies_analytics = []
 
     for programming_language in PROGRAMMING_LANGUAGES:
@@ -103,7 +105,7 @@ def get_sj_vacancies_data(sj_api_key):
 
                 if salary is not None:
                     paid_vacancies_for_language += 1
-                    accumulated_salary += predict_rub_salary_sj(vacancy)
+                    accumulated_salary += salary
 
         if paid_vacancies_for_language != 0:
             median_salary = int(accumulated_salary / paid_vacancies_for_language)
@@ -112,7 +114,7 @@ def get_sj_vacancies_data(sj_api_key):
     return vacancies_analytics
 
 
-def get_hh_vacancies_data():
+def get_hh_vacancies():
     vacancies_analytics = []
 
     for language in PROGRAMMING_LANGUAGES:
@@ -132,7 +134,7 @@ def get_hh_vacancies_data():
 
                 if salary is not None:
                     vacancies_for_language += 1
-                    accumulated_salary += predict_rub_salary_hh(api_response['items'][item]['salary'])
+                    accumulated_salary += salary
 
         median_salary = int(accumulated_salary / vacancies_for_language)
         vacancies_analytics.append(
@@ -157,8 +159,8 @@ def main():
     sj_api_key = os.getenv('SJ_API_SECRET_KEY')
     sj_title = 'SuperJob Moscow'
     hh_title = 'HeadHunter Moscow'
-    create_terminal_table(get_sj_vacancies_data(sj_api_key), sj_title)
-    create_terminal_table(get_hh_vacancies_data(), hh_title)
+    create_terminal_table(get_sj_vacancies(sj_api_key), sj_title)
+    create_terminal_table(get_hh_vacancies(), hh_title)
 
 
 if __name__ == "__main__":
